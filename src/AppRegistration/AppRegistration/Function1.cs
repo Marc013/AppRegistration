688169c1d27a8e76b573b3b4be6e68f796b2f3a2
Marc van Gorp <Marc.van.Gorp@infield-ict.nl>
2023-10-25 20:37:42 +0200

    Initial function AppRegistration

---
+++ src/AppRegistration/AppRegistration/Function1.cs
@@ -0,0 +1,25 @@
+using System;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AppRegistration
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public void Run([ServiceBusTrigger("AppRegistrationCreate", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+        }
+    }
+}
