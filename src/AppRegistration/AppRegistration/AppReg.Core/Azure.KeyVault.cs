bbf9ee6faae8734bf9ee28c4f173cf914dcbc8d8
Marc van Gorp <Marc.van.Gorp@infield-ict.nl>
2023-10-28 12:57:38 +0200

    First start at retrieving secret from key vault (not functional)

---
+++ src/AppRegistration/AppRegistration/AppReg.Core/Azure.KeyVault.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AppRegistration.AppReg.Core
+{
+    internal class AzureKeyVault (string secretName)
+    {
+        readonly string keyVaultName = "%KEY_VAULT_NAME%"; //Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+        readonly string kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+        var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+        var secret = await client.GetSecretAsync(secretName); // to be used in the function app itself
+    }
+}
