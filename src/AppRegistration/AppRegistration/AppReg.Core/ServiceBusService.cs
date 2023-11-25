48cde27c7f70ed12df7a88eac5ad41730d8d6f20
Marc van Gorp <Marc.van.Gorp@infield-ict.nl>
2023-11-25 13:59:23 +0100

    Removed unnecessary comments

--- src/AppRegistration/AppRegistration/AppReg.Core/ServiceBusService.cs
+++ src/AppRegistration/AppRegistration/AppReg.Core/ServiceBusService.cs
@@ -21,22 +21,10 @@ namespace AppRegistration.AppReg.Core
             var queueName = Environment.GetEnvironmentVariable("ServiceBusQueueName");
             var nameSpace = Environment.GetEnvironmentVariable("ServiceBusNamespace");
 
-            // name of your Service Bus queue
-            // the client that owns the connection and can be used to create senders and receivers
             ServiceBusClient client;
 
-            // the sender used to publish messages to the queue
             ServiceBusSender sender;
 
-            // number of messages to be sent to the queue
-            const int numOfMessages = 3;
-
-            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
-            // of the application, which is best practice when messages are being published or read
-            // regularly.
-            //
-            // Set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
-            // If you use the default AmqpTcp, ensure that ports 5671 and 5672 are open.
             var clientOptions = new ServiceBusClientOptions
             {
                 TransportType = ServiceBusTransportType.AmqpWebSockets
@@ -53,14 +41,11 @@ namespace AppRegistration.AppReg.Core
 
             try
             {
-                // sent single message
                 await sender.SendMessageAsync(new ServiceBusMessage(message));
                 _logger.LogInformation("Sending message '{message}' to the service bus queue.", message);
             }
             finally
             {
-                // Calling DisposeAsync on client types is required to ensure that network
-                // resources and other unmanaged objects are properly cleaned up.
                 await sender.DisposeAsync();
                 await client.DisposeAsync();
             }
